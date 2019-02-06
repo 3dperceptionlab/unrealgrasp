4eb4e0357d625aa2ade82f084efa64d5d968e644
Sergiu Oprea <soprea@dtic.ua.es>
2019-02-06 10:58:59 +0100

    Basic logic to export a csv file with grasp info
    We used Victory as external plugin for exporting data to csv.

---
+++ Plugins/VictoryPlugin19/Output19/Intermediate/Build/Win32/UE4/Inc/VictoryBPLibrary/VictoryISM.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VICTORYBPLIBRARY_VictoryISM_generated_h
+#error "VictoryISM.generated.h already included, missing '#pragma once' in VictoryISM.h"
+#endif
+#define VICTORYBPLIBRARY_VictoryISM_generated_h
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_RPC_WRAPPERS
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVictoryISM(); \
+	friend VICTORYBPLIBRARY_API class UClass* Z_Construct_UClass_AVictoryISM(); \
+public: \
+	DECLARE_CLASS(AVictoryISM, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/VictoryBPLibrary"), NO_API) \
+	DECLARE_SERIALIZER(AVictoryISM) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAVictoryISM(); \
+	friend VICTORYBPLIBRARY_API class UClass* Z_Construct_UClass_AVictoryISM(); \
+public: \
+	DECLARE_CLASS(AVictoryISM, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/VictoryBPLibrary"), NO_API) \
+	DECLARE_SERIALIZER(AVictoryISM) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVictoryISM(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVictoryISM) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVictoryISM); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVictoryISM); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVictoryISM(AVictoryISM&&); \
+	NO_API AVictoryISM(const AVictoryISM&); \
+public:
+
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVictoryISM(AVictoryISM&&); \
+	NO_API AVictoryISM(const AVictoryISM&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVictoryISM); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVictoryISM); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVictoryISM)
+
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_PRIVATE_PROPERTY_OFFSET
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_12_PROLOG
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_RPC_WRAPPERS \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_INCLASS \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_INCLASS_NO_PURE_DECLS \
+	HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_VictoryBPLibrary_Source_VictoryBPLibrary_Public_VictoryISM_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
